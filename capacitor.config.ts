d35551be61f98c5f95353f8354fc0fffd06d213e
williamnguetsop <wnguetsop@gmail.com>
2026-02-21 00:30:42 +0100

    Fix camera, upload e navigazione interna

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,9 +12,18 @@ const config: CapacitorConfig = {
       'mastershoppro.com',
       '*.mastershoppro.com',
       '*.stripe.com',
-      '*.firebaseapp.com'
+      '*.firebaseapp.com',
+      'accounts.google.com' // Necessario per il login Google
     ]
-  }
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      // Il tuo Web Client ID (client_type: 3) dal file JSON
+      serverClientId: '243302666157-je0b9se443rdv4hkup88bll6vg4ofu3h.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
